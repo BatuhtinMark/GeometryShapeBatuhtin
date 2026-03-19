903e96c2839a5643505569d8ddf3106e510c7914
Mark <markbatuhtin7@icloud.com>
2026-03-19 23:33:19 +0500

    Fix Triangle Area test

--- Desktop/GeometryShape/GeometryShape.Tests/TriangleTests.cs
+++ Desktop/GeometryShape/GeometryShape.Tests/TriangleTests.cs
@@ -23,7 +23,7 @@ namespace GeometryShape.Tests
         public void Area_CalculatedCorrectly()
         {
             var tri = new Triangle(3, 4, 5);
-            Assert.Equal(7, tri.Area());
+            Assert.Equal(6, tri.Area());
         }
 
         [Fact]
